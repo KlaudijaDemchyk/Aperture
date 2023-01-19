44f3826360ecb54968b5437b496e5c9097cda91a
KlaudijaDemchyk <fkst@i.ua>
2023-01-19 20:42:19 +0100

    dialog

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,27 @@
 $(document).ready(function(){
+
+    $('#open').click(function(){
+        $('.model-container').css('transform','scale(1)');
+});
+    $('#close').click(function(){
+        $('.model-container').css('transform','scale(0)');
+    });
+
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        navText: [ '', ' ' ],
-        autoplay:true,
-        autoplayTimeout:2000,
-        responsive:{
-            0:{
-                items:2
+        loop: true,
+        margin: 0,
+        nav: true,
+        navText: ['', ' '],
+        autoplay: true,
+        autoplayTimeout: 2000,
+        responsive: {
+            0: {
+                items: 2
             },
-            1000:{
-                items:5
+            1000: {
+                items: 5
             }
         }
     });
+
 });
